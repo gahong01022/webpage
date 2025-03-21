ae3faa99c0fb39456d3021a9e545cae8a61a0dda
Windcool <62234010+gahong01022@users.noreply.github.com>
2025-03-22 01:05:24 +0800

    Initial commit

---
+++ Website/index.js
@@ -0,0 +1,74 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') {
+        filePath = './main-page.html';
+    }
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    let contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.gif':
+            contentType = 'image/gif';
+            break;
+        case '.svg':
+            contentType = 'image/svg+xml';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+        case '.mp4':
+            contentType = 'video/mp4';
+            break;
+        case '.woff':
+            contentType = 'application/font-woff';
+            break;
+        case '.ttf':
+            contentType = 'application/font-ttf';
+            break;
+        case '.eot':
+            contentType = 'application/vnd.ms-fontobject';
+            break;
+        case '.otf':
+            contentType = 'application/font-otf';
+            break;
+    }
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if (error.code == 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>404 Not Found</h1>', 'utf-8');
+            } else {
+                res.writeHead(500);
+                res.end('Sorry, there was an error: ' + error.code + ' ..\n', 'utf-8');
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
